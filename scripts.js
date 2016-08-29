179eb7ad782b59d1d5b97762b1d49b7425943d88
Andreas Aubell <Andreas Aubell>
2016-08-29 23:25:10 +0200

    print preview mostly

--- scripts.js
+++ scripts.js
@@ -3,10 +3,6 @@ var countSelectedImages = 0;
 var selectedImages = [];
 
 function AddToPrint(filename, noPush){
-    //var dialog = document.getElementById("printDialog");
-    //dialog.classList.remove("hidden");
-
-    //imgContainerSelected
 
     if(!noPush)
         selectedImages.push(filename);
@@ -55,19 +51,81 @@ function OnLoadPrintPage(){
 
     var imgContainer = document.getElementById("imgContainer");
 
-    for(var i = 0; i < selectedImages.length; i++){
+    var numberOfImagesAddedController = 0;
+    var createAdRowNow = false;
+    var createNewImageRow = true;
+    var currentImageRow = undefined;
 
-        var createdColumnDiv = document.createElement("div");
-        createdColumnDiv.classList.add("col-2"); 
+    for(var i = numberOfImagesAddedController; i < selectedImages.length; i++){
+        if(createNewImageRow){
+            currentImageRow = document.createElement("div");
+            currentImageRow.classList.add("row");
+
+            imgContainer.appendChild(currentImageRow);
+
+            createNewImageRow = false;
+        }
+
+        var createdImageColumnDiv = CreateColDiv("3");
+        createdImageColumnDiv.classList.add("printPreviewColumn");
+
 
         var createdImg = document.createElement("img");
         createdImg.src = selectedImages[i];
         createdImg.classList.add("downloadableImage");
+        
 
-        createdColumnDiv.appendChild(createdImg);
+        var printPreviewDiv = document.createElement("div");
+        printPreviewDiv.classList.add("printPreview");
+        
 
-        imgContainer.appendChild(createdColumnDiv);
+        printPreviewDiv.appendChild(createdImg);
+        
+
+        createdImageColumnDiv.appendChild(printPreviewDiv);
+        currentImageRow.appendChild(createdImageColumnDiv);
+
+        numberOfImagesAddedController++;
+
+        if(numberOfImagesAddedController == 4){
+            createNewImageRow = true;
+            numberOfImagesAddedController = 0;
+
+            //ads row after evert 4th image
+            imgContainer.appendChild(CreateAdRow());
+        }
+
+        //if last
+        if(i+1 == selectedImages.length){
+            var numberOfEmtyDivsToAdd = 4 - numberOfImagesAddedController;
+
+            for(var y = 0; y < numberOfEmtyDivsToAdd; y++){
+                currentImageRow.appendChild(CreateColDiv("3"));
+            }
+        }
     }
+
+    imgContainer.appendChild(CreateAdRow());
+}
+
+function CreateColDiv(dementor){
+    var createdColumnDiv = document.createElement("div");
+    createdColumnDiv.classList.add("col-" + dementor);    
+
+    return createdColumnDiv;
+}
+
+function CreateAdRow(){
+    var adColumn = CreateColDiv(12);
+    adColumn.style = "background-color:SteelBlue; color:white";
+    adColumn.innerHTML = "Placeholder for ads <br/><br/><br/><br/><br/>"
+
+    var adRow = document.createElement("div");
+    adRow.classList.add("row");
+    adRow.classList.add("ad");
+    adRow.appendChild(adColumn);
+
+    return adRow;
 }
 
 function OpenPrintDialog(){
